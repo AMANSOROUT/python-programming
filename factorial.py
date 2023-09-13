0efadd8943eb5f0164e89b5cdfebf4702879d8d4
AMANSOROUT <142568090+AMANSOROUT@users.noreply.github.com>
2023-09-13 12:21:23 +0530

    factorial . py

---
+++ factorial.py
@@ -0,0 +1,23 @@
+#factorial of given No.
+
+n= int(input("enter the Number"))
+
+s=0
+
+for i in range(1,n+1):
+    if n%1==0:
+        
+        s=s+i
+
+print (f' sum of all divisor of {n} is {s}')
+       
+
+
+
+
+
+
+
+
+
+
