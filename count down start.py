5151bf1209f5ad8c28fedcf47b2ab5e2291f586e
AMANSOROUT <142568090+AMANSOROUT@users.noreply.github.com>
2023-09-19 22:01:08 +0530

    count-down.py

---
+++ count down start.py
@@ -0,0 +1,12 @@
+#question no. 1
+
+import time
+n=int(input('Enter the no.'))
+print('counter rhe time')
+while n:
+    print(n)
+    n-=1
+    time.sleep(1)
+else:
+    print('counter stop')
+               
